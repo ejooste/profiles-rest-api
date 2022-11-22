cd548cbf2f751e88e276ab5c9451970f36fd58fc
EJooste <ejooste@outlook.com>
2022-11-22 11:22:07 -0800

    Model build out and DB connect
    Updated the custom model design, and update the Database connections.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,8 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self, email, name, passwork=None):
+        """Crete a new User Profile"""
+        if not email:
+            raise ValueError("User must a valid have email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create and Save a new superuser with givin details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database Model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
@@ -10,7 +40,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserProfileManger()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQURIED_FIELDS = ['name']
@@ -25,4 +55,4 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         """Retrieve string representation of user"""
-            return self.email
+        return self.email
